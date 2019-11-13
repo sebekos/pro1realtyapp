8bedf472bbabd0ca6e3ceef135204dd286316f21
unknown <sebkpl@gmail.com>
2019-11-12 21:22:22 -0600

    CSS cleanup

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,71 +1,75 @@
-import React, { Fragment, useState } from 'react';
-import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { login } from '../../Redux/actions/auth';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { login } from "../../Redux/actions/auth";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 const Login = ({ login, isAuthenticated }) => {
-    const [formData, setFormData] = useState({
-        email: '',
-        password: ''
-    });
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
 
-    const { email, password } = formData;
+  const { email, password } = formData;
 
-    const onChangeHandler = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onChangeHandler = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onSubmitHandler = async e => {
-        e.preventDefault();
-        login(email, password);
-    }
+  const onSubmitHandler = async e => {
+    e.preventDefault();
+    login(email, password);
+  };
 
-    // Redirect if logged in
-    if (isAuthenticated) {
-        return <Redirect to='/myprofile' />
-    }
+  // Redirect if logged in
+  if (isAuthenticated) {
+    return <Redirect to="/myprofile" />;
+  }
 
-    return (
-        <Fragment>
-            <h1 className="large text-primary">Sign In</h1>
-            <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
-            <form className="form" onSubmit={onSubmitHandler}>
-                <div className="form-group">
-                    <input
-                        type="email"
-                        placeholder="Email Address"
-                        name="email"
-                        value={email}
-                        onChange={onChangeHandler}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        minLength="6"
-                        value={password}
-                        onChange={onChangeHandler}
-                        required
-
-                    />
-                </div>
-                <input type="submit" className="btn btn-primary" value="Login" />
-                <Link to='/pwreset'><small>Forgot password?</small></Link>
-            </form>
-        </Fragment>
-    )
-}
+  return (
+    <div className="login-container">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={onSubmitHandler}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChangeHandler}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={onChangeHandler}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+        <Link to="/pwreset">
+          <small>Forgot password?</small>
+        </Link>
+      </form>
+    </div>
+  );
+};
 
 Login.propTypes = {
-    login: PropTypes.func.isRequired,
-    isAuthenticated: PropTypes.bool
-}
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
 
 const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated
+  isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(mapStateToProps, { login })(Login);
--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,30 +1,61 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import NumberFormat from 'react-number-format';
+import React from "react";
+import { Link } from "react-router-dom";
+import NumberFormat from "react-number-format";
 
-const Profile = ({ profile: { name, position, location, phone, email, photo, user, loading } }) =>
-    <div className="profiles">
-        <div className="profile bg-light">
-            <img
-                className="avatar-icon"
-                src={photo ? photo : ""}
-                alt=""
+const Profile = ({
+  profile: { name, position, location, phone, email, photo, user, loading }
+}) => (
+  <div className="profiles">
+    <div className="profile bg-light">
+      <img
+        className="avatar-icon"
+        src={
+          photo
+            ? photo
+            : "https://pro1realty.s3.us-east-2.amazonaws.com/avatars/1565831781133.png"
+        }
+        alt=""
+      />
+      <div>
+        <h2>{name}</h2>
+        {position ? (
+          <p className="p-important">
+            <span className="span-item"></span>
+            {position}
+          </p>
+        ) : null}
+        {location ? (
+          <p>
+            <span className="span-item">Location: </span>
+            {location}
+          </p>
+        ) : null}
+        {phone ? (
+          <p>
+            <span className="span-item">Phone: </span>
+            <NumberFormat
+              displayType="text"
+              format="(###) ###-####"
+              value={phone}
             />
-            <div>
-                <h2>{name}</h2>
-                {position ? <p className='p-important'><span className='span-item'></span>{position}</p> : null}
-                {location ? <p><span className='span-item'>Location: </span>{location}</p> : null}
-                {phone ? <p><span className='span-item'>Phone: </span><NumberFormat
-                    displayType="text"
-                    format="(###) ###-####"
-                    value={phone}
-                /></p> : null}
-                {email ? <p><span className='span-item'>Email: </span>{email}</p> : null}
-            </div>
-            <div>
-                {!loading ? <Link className="btn btn-primary my-1" to={`/listings/${user}`}>View Listings</Link> : null}
-            </div>
-        </div>
+          </p>
+        ) : null}
+        {email ? (
+          <p>
+            <span className="span-item">Email: </span>
+            {email}
+          </p>
+        ) : null}
+      </div>
+      <div>
+        {!loading ? (
+          <Link className="btn btn-primary my-1" to={`/listings/${user}`}>
+            View Listings
+          </Link>
+        ) : null}
+      </div>
     </div>
+  </div>
+);
 
 export default Profile;
