40b28e02a0bbc4969ef10759413d83260c20737f
sebekos <sebkpl@gmail.com>
2020-03-28 12:41:38 -0500

    Fixed my listings

--- client/src/components/dashboards/MyListings.js
+++ client/src/components/dashboards/MyListings.js
@@ -1,13 +1,14 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getRefinedListings } from "../../Redux/actions/listing";
 import { getProfile } from "../../Redux/actions/profile";
-import ListingSummaryItem from "../listing/ListingSummaryItem";
+import Pagination from "../listing/Pagination";
+import SearchBar from "../listing/SearchBar";
 
-const Dashboard = ({ auth: { user }, listing: { listings }, profile: { profile }, getRefinedListings, getProfile }) => {
+const Dashboard = ({ auth: { user }, listing: { listings, loading, pages }, profile: { profile }, getRefinedListings, getProfile }) => {
     useEffect(() => {
         getProfile();
         if (user) {
@@ -16,6 +17,33 @@ const Dashboard = ({ auth: { user }, listing: { listings }, profile: { profile }
             });
         }
     }, [user]);
+
+    const [formData, setFormData] = useState({
+        zipcode: "",
+        type: "Newest",
+        group: "All",
+        agentid: "",
+        page: "0"
+    });
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSearch = () => {
+        const sendData = {
+            ...formData,
+            agentid: user._id
+        };
+        getRefinedListings(sendData);
+    };
+
+    const pageClick = data => {
+        const sendData = {
+            ...formData,
+            agentid: user._id,
+            page: data.selected
+        };
+        getRefinedListings(sendData);
+    };
 
     return (
         <Fragment>
@@ -36,21 +64,12 @@ const Dashboard = ({ auth: { user }, listing: { listings }, profile: { profile }
                     </Link>
                 </Fragment>
             )}
-            {listings ? (
-                <Fragment>
-                    <div className="posts">
-                        {listings.length > 0
-                            ? listings.map(listing => (
-                                  <ListingSummaryItem key={listing._id} listing={listing} listingId={listing._id}>
-                                      Listing
-                                  </ListingSummaryItem>
-                              ))
-                            : null}
-                    </div>
-                </Fragment>
-            ) : (
-                <Spinner />
-            )}
+            <div className="listings-view">
+                {loading ? <Spinner /> : null}
+                <SearchBar onChange={onChange} onSearch={onSearch} data={formData} />
+                {listings.length > 0 ? <Pagination pageClick={pageClick} pages={pages} listings={listings} /> : null}
+                {!loading && listings.length === 0 ? <div className="text-center">No listings matching criteria</div> : null}
+            </div>
         </Fragment>
     );
 };
