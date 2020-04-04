1359346fd3dd97ba7e04679afc6ec666512e7252
sebekos <sebkpl@gmail.com>
2020-04-04 15:48:57 -0500

    merge

--- routes/api/user.js
+++ routes/api/user.js
@@ -18,14 +18,10 @@ const Pwreset = require("../../models/Pwreset");
 router.post(
     "/",
     [
-        check("name", "Name is required")
-            .not()
-            .isEmpty(),
+        check("name", "Name is required").not().isEmpty(),
         check("email", "Password is required").isEmail(),
         check("password", "Please enter a password with 6 or more characters").isLength({ min: 6 }),
-        check("registerkey", "Key is required")
-            .not()
-            .isEmpty()
+        check("registerkey", "Key is required").not().isEmpty(),
     ],
     async (req, res) => {
         const errors = validationResult(req);
@@ -46,7 +42,7 @@ router.post(
             user = new User({
                 name,
                 email,
-                password
+                password,
             });
             // Encrypt password
             const salt = await bcrypt.genSalt(10);
@@ -57,8 +53,8 @@ router.post(
 
             const payload = {
                 user: {
-                    id: user.id
-                }
+                    id: user.id,
+                },
             };
 
             jwt.sign(payload, process.env.jwtSecret, { expiresIn: 360000 }, (err, token) => {
@@ -74,74 +70,65 @@ router.post(
 // @route       POST api/user/pwreset
 // @description Setup reset
 // @access      Public
-router.post(
-    "/pwreset",
-    [
-        check("email", "Email is required")
-            .not()
-            .isEmpty()
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        const email = req.body.email;
-        let user = await User.findOne({ email: email });
-        if (!user) {
-            return res.status(400).json({ errors: [{ msg: "Server Error" }] });
-        }
-
-        // Check attempts
-        const pwresetcheck = await Pwreset.findOne({ email });
-        if (pwresetcheck && pwresetcheck.attempts >= 3) {
-            return res.status(400).json({ errors: [{ msg: "Account disabled, please contact the admin to reset your password" }] });
-        }
-
-        // Delete previous reset links
-        await Pwreset.deleteMany({ email: email });
-
-        pwreset = new Pwreset({
-            email: email,
-            hash: ""
-        });
-
-        // Encrypt key
-        const salt = await bcrypt.genSalt(10);
-        // Random bytes
-        const random = await randomBytes(10).toString("hex");
-        // Hash key
-        pwreset.hash = await bcrypt.hash(random, salt);
-        // Save
-        await pwreset.save();
-
-        var transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: process.env.PW_RESET_EMAIL,
-                pass: process.env.PW_RESET_PW
-            },
-            secure: false,
-            tls: { rejectUnauthorized: false }
-        });
-
-        var mailOptions = {
-            from: process.env.PW_RESET_EMAIL,
-            to: email,
-            subject: "Pro 1 Realty Reset",
-            text: `Follow the link below to reset your password. http://pro1mainst.com/pwresetsave/${random}`
-        };
-
-        transporter.sendMail(mailOptions, function(error, info) {
-            if (error) {
-                return res.json({ msg: "Error sending email" });
-            } else {
-                return res.json({ msg: "Reset email sent" });
-            }
-        });
+router.post("/pwreset", [check("email", "Email is required").not().isEmpty()], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
-);
+
+    const email = req.body.email;
+    let user = await User.findOne({ email: email });
+    if (!user) {
+        return res.status(400).json({ errors: [{ msg: "Server Error" }] });
+    }
+
+    // Check attempts
+    const pwresetcheck = await Pwreset.findOne({ email });
+    if (pwresetcheck && pwresetcheck.attempts >= 3) {
+        return res.status(400).json({ errors: [{ msg: "Account disabled, please contact the admin to reset your password" }] });
+    }
+
+    // Delete previous reset links
+    await Pwreset.deleteMany({ email: email });
+
+    pwreset = new Pwreset({
+        email: email,
+        hash: "",
+    });
+
+    // Encrypt key
+    const salt = await bcrypt.genSalt(10);
+    // Random bytes
+    const random = await randomBytes(10).toString("hex");
+    // Hash key
+    pwreset.hash = await bcrypt.hash(random, salt);
+    // Save
+    await pwreset.save();
+
+    var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.PW_RESET_EMAIL,
+            pass: process.env.PW_RESET_PW,
+        },
+    });
+
+    var mailOptions = {
+        from: process.env.PW_RESET_EMAIL,
+        to: email,
+        subject: "Pro 1 Realty Reset",
+        text: `Follow the link below to reset your password. http://pro1mainst.com/pwresetsave/${random}`,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+            return res.json({ msg: "Error sending email" });
+        } else {
+            return res.json({ msg: "Reset email sent" });
+        }
+    });
+});
 
 // @route       POST api/user/confirmreset
 // @description Reset password
@@ -151,9 +138,7 @@ router.post(
     [
         check("email", "Email is required").isEmail(),
         check("password", "Please enter a password with 6 or more characters").isLength({ min: 6 }),
-        check("hash", "Hash is required")
-            .not()
-            .isEmpty()
+        check("hash", "Hash is required").not().isEmpty(),
     ],
     async (req, res) => {
         const errors = validationResult(req);
