9414c858a8fb8cfc6c383d4c78219c82886cfc5d
unknown <sebkpl@gmail.com>
2024-08-02 17:04:12 -0500

    background

--- client2/src/components/about/Landing.jsx
+++ client2/src/components/about/Landing.jsx
@@ -3,7 +3,12 @@ import "./styles.css";
 
 const Landing = () => {
   return (
-    <div className="landing-page">
+    <div
+      className="landing-page"
+      style={{
+        backgroundImage: `url("./landing.jpg")`,
+      }}
+    >
       <img src={`./pro1realtylogo.png`} alt="" className="landing-page-logo" />
       <h1 className="landing-page-title">
         List your home or find a new one today
