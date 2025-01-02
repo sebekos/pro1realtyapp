054af4d7daad49eb0030ddca928ee8684db30703
unknown <sebkpl@gmail.com>
2025-01-02 07:44:54 -0600

    gg

--- client2/src/components/login/Login.jsx
+++ client2/src/components/login/Login.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({ username: "", password: "" });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Username: ${formData.username}, Password: ${formData.password}`);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-form-container">
+        <form className="login-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={formData.username}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
